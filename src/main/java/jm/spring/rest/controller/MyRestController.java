69362b8d19bb2110adad58659646a860ad045b30
dmtch-github <dmtch@yandex.ru>
2021-12-27 19:21:34 +0300

    Осталось отладить сохранение и изменение юзера с сайта через REST

--- src/main/java/jm/spring/rest/controller/MyRestController.java
+++ src/main/java/jm/spring/rest/controller/MyRestController.java
@@ -23,6 +23,14 @@ public class MyRestController {
         this.userService = userService;
     }
 
+
+    @GetMapping("/test/{id}")
+    public String  getTest(@PathVariable int id) {
+        System.out.println(">>> getTest - вернул Answer"+id);
+        return "Answer"+id;
+    }
+
+
     @GetMapping("/users")
     public List<User> getUsers() {
         System.out.println(">>> MyRestController.getUsers обработал запрос ВСЕ ЮЗЕРЫ");
@@ -34,31 +42,34 @@ public class MyRestController {
         System.out.println(">>> MyRestController.getOneUser обработал запрос ЮЗЕР с id="+id);
         User user = userService.getUser(id);
         if(user == null) {
-            throw new NoDataFoundException("User with id=" + id + " not found into DB");
+            throw new NoDataFoundException("ERROR: User with id=" + id + " not found into DB");
         }
         return user;
     }
 
     @PostMapping("/users") //создание нового юзера
     public User saveUser(@RequestBody User user) {
+        System.out.println("POST: Сохраню юзера "+user);
         userService.saveUser(user);
         return user;
     }
 
     @PutMapping("/users") //изменение существующего юзера
     public User updateUser(@RequestBody User user) {
+        System.out.println("PUT: Изменю юзера с id="+user.getId());
         userService.saveUser(user);
         return user;
     }
 
     @DeleteMapping("/users/{id}")
     public String deleteUser(@PathVariable int id) {
+        System.out.println("Буду удалять юзера с номером "+id);
         User user = userService.getUser(id);
         if(user == null) {
-            throw new NoDataFoundException("User with id=" + id + " not found into DB");
+            throw new NoDataFoundException("ERROR: User with id=" + id + " not found into DB");
         }
         userService.deleteUser(id);
-        return "User with id=" + id + " was deleted from DB";
+        return "OK. User with id=" + id + " was deleted from DB";
     }
 
 
