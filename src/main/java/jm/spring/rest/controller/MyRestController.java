cb10fb570870d12fc5241c0a632c3b51e2228779
dmtch-github <dmtch@yandex.ru>
2021-12-28 11:34:22 +0300

    Причесал код

--- src/main/java/jm/spring/rest/controller/MyRestController.java
+++ src/main/java/jm/spring/rest/controller/MyRestController.java
@@ -24,22 +24,13 @@ public class MyRestController {
     }
 
 
-    @GetMapping("/test/{id}")
-    public String  getTest(@PathVariable int id) {
-        System.out.println(">>> getTest - вернул Answer"+id);
-        return "Answer"+id;
-    }
-
-
     @GetMapping("/users")
     public List<User> getUsers() {
-        System.out.println(">>> MyRestController.getUsers обработал запрос ВСЕ ЮЗЕРЫ");
         return userService.getUsers();
     }
 
     @GetMapping("/users/{id}")
     public User getOneUser(@PathVariable int id) {
-        System.out.println(">>> MyRestController.getOneUser обработал запрос ЮЗЕР с id="+id);
         User user = userService.getUser(id);
         if(user == null) {
             throw new NoDataFoundException("ERROR: User with id=" + id + " not found into DB");
@@ -49,22 +40,18 @@ public class MyRestController {
 
     @PostMapping("/users") //создание нового юзера
     public User saveUser(@RequestBody User user) {
-        System.out.println("POST: Сохраню юзера "+user);
         userService.saveUser(user);
-        System.out.println("POST: Сохранил юзера "+user);
         return user;
     }
 
     @PutMapping("/users") //изменение существующего юзера
     public User updateUser(@RequestBody User user) {
-        System.out.println("PUT: Изменю юзера с id="+user.getId());
         userService.saveUser(user);
         return user;
     }
 
     @DeleteMapping("/users/{id}")
     public String deleteUser(@PathVariable int id) {
-        System.out.println("Буду удалять юзера с номером "+id);
         User user = userService.getUser(id);
         if(user == null) {
             throw new NoDataFoundException("ERROR: User with id=" + id + " not found into DB");
